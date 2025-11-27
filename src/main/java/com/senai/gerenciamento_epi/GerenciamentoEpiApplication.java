54e8864df8dc01ceca80d62dbc00255af7189234
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2025-11-26 23:24:15 -0300

    chore: configuração inicial do projeto via spring initializr

---
+++ src/main/java/com/senai/gerenciamento_epi/GerenciamentoEpiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.gerenciamento_epi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoEpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoEpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/gerenciamento_epi/GerenciamentoEpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.gerenciamento_epi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
